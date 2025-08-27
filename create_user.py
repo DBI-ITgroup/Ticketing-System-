4923f6502505c8ee42315240b6ec485086b0e8cc
DBI-ITgroup <ITSupport@dbi-itgroup.co.za>
2025-08-27 18:34:23 +0200

    Added multiple users

--- create_user.py
+++ create_user.py
@@ -23,3 +23,49 @@ if not CustomUser.objects.filter(username=username).exists():
     print(f"Superuser {username} created successfully.")
 else:
     print(f"Superuser {username} already exists.")
+
+# --- NORMAL USERS ---
+users_data = [
+    {
+        "username": "Sinelizwi.Gebeda@dbi-itgroup.co.za",
+        "email": "Sinelizwi.Gebeda@dbi-itgroup.co.za",
+        "password": "Katlego@25",
+        "full_name": "Sine DBI",
+        "role": "End-User",
+    },
+    {
+        "username": "hhelpdesk83@gmail.com",
+        "email": "hhelpdesk83@gmail.com",
+        "password": "Katlego@25",
+        "full_name": "Technician L1-1",
+        "role": "L1_Technician",
+    },
+  {
+        "username": "sinelizwigebeda9@gmail.com",
+        "email": "sinelizwigebeda9@gmail.com",
+        "password": "Katlego@25",
+        "full_name": "Technician L1-2",
+        "role": "L1_Technician",
+    },
+     {
+        "username": "Training@dbi-itgroup.co.za",
+        "email": "Training@dbi-itgroup.co.za",
+        "password": "Katlego@25",
+        "full_name": "Technician L2-1",
+        "role": "L2_Technician",
+    },
+     {
+        "username": "ITSupport@dbi-itgroup.co.za",
+        "email": "ITSupport@dbi-itgroup.co.za",
+        "password": "Katlego@25",
+        "full_name": "Technician L2-2",
+        "role": "L2_Technician",
+    }
+    ]
+
+for user in users_data:
+    if not CustomUser.objects.filter(username=user["username"]).exists():
+        CustomUser.objects.create_user(**user)
+        print(f"User {user['username']} created successfully.")
+    else:
+        print(f"User {user['username']} already exists.")
